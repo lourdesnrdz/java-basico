d583a679940018bc5796717a3745d8d60e0c7200
lourdesnrdz <lourdesnrdz@gmail.com>
2021-06-26 18:43:47 -0500

    Camel Case

--- src/NamingJava.java
+++ src/NamingJava.java
@@ -1,3 +1,4 @@
+            //Upper Camel Case
 public class NamingJava {
     public static void main(String[] args) {
         int cellphone = 33337777;
@@ -15,5 +16,10 @@ public class NamingJava {
         final int POSITION = -5;
         final int MAX_WIDTH = 9999;
         final int MIN_WIDTH = 1;
+
+        //Lower Camel Case
+        String fullName = "Lourdes Navarrete Rodriguez";
+        int sizeInCentimeters = 26;
+
     }
 }
