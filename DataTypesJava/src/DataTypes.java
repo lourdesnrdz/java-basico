95b9ac2a8ecd0c22d313b1b1c79c30651dbd98dd
lourdesnrdz <lourdesnrdz@gmail.com>
2021-06-26 20:37:25 -0500

    math operations

--- DataTypesJava/src/DataTypes.java
+++ DataTypesJava/src/DataTypes.java
@@ -1,8 +1,23 @@
 public class DataTypes {
 
-    int n = 1234567890;
-    long nL = 12345678901L;
-    double nD = 123.456;
-    float nf = 123.456F;
+    public static void main(String[] args) {
+        int n = 1234567890;
+        long nL = 12345678901L;
+
+        double nD = 123.456;
+        float nf = 123.456F;
+
+        var salary = 1000; //int
+        //pension 3%
+        var pension = salary * 0.03; //double
+        var totalSalary = salary - pension; //double
+
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeeName = "Lourdes Navarrete";
+        System.out.println("Employee: " + employeeName + " Salary: " + totalSalary);
+    }
 
 }
