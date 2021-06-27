1f44aaa68583adfb3664a13eef4053c593093065
lourdesnrdz <lourdesnrdz@gmail.com>
2021-06-27 14:56:45 -0500

    logic operators and boolean expressions

--- HolaMundoJava/src/IfStatement.java
+++ HolaMundoJava/src/IfStatement.java
@@ -1,12 +1,18 @@
 public class IfStatement {
     public static void main(String[] args) {
-        boolean isBluetoothEnabled = true;
+        boolean isBluetoothEnabled = false;
         int filesSent = 3;
 
         if (isBluetoothEnabled){
             //Send file
             filesSent++;
             System.out.println("File sent");
+        } else {
+            filesSent--;
+            System.out.println("Please enable Bluetooth");
         }
+
+        System.out.println(isBluetoothEnabled);
+        System.out.println(filesSent);
     }
 }
