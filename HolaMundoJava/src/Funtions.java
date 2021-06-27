fda62f131a6c8b1d4e773a7c3a36ccd195e66af6
lourdesnrdz <lourdesnrdz@gmail.com>
2021-06-27 15:48:01 -0500

    java docs

--- HolaMundoJava/src/Funtions.java
+++ HolaMundoJava/src/Funtions.java
@@ -18,18 +18,39 @@ public class Funtions {
         System.out.println("Pesos a Dolares: " + convertToDolar(1000, "COP"));
     }
 
+    /**
+     * Calculate the area of a circle
+     * @param r radius value
+     * @return value of area of circle
+     */
     public static double circleArea(double r) {
         return Math.PI * Math.pow(r, 2);
     }
 
+    /**
+     * Calculate the area of a sphere
+     * @param r radius value
+     * @return value of area of sphere
+     */
     public static double sphereArea(double r) {
         return 4 * Math.PI * Math.pow(r, 2);
     }
 
+    /**
+     * Calculate the volume of a sphere
+     * @param r radius value
+     * @return value of volume of sphere
+     */
     public static double sphereVolume(double r) {
         return (4/3) * Math.PI * Math.pow(r, 3);
     }
 
+    /**
+     * Description: Función que especificando su moneda convierte una cantidad de dinero a dolares
+     * @param quantity Cantidad de dinero
+     * @param currency Tipo de moneda: Solo acepta MXN o COP
+     * @return quantity Devuelve la cantidad actualizada en Dolares
+     */
     public static double convertToDolar(double quantity, String currency) {
         switch (currency) {
             case "MXN":
